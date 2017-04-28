f0d87ff0293c3d8b7a7db13124e7d6136c798924
Dhawal Joharapurkar <dmjan21@gmail.com>
2017-04-28 13:38:11 -0700

    [incomplete] script to extract review text mentions

--- code/script_reviewTextMentions.py
+++ code/script_reviewTextMentions.py
@@ -1,3 +1,12 @@
+import json
 import os
 import sys
 
+def load_data():
+	with open(os.path.join("..", "data", "Yelp")) as data_file:
+		data = json.load(data_file)
+
+def main():
+
+if __name__ == '__main__':
+	main()
