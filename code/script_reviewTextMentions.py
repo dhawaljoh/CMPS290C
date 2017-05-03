a8cfe103597c59de3e6269c7418765b342a6226b
Dhawal Joharapurkar <dhawal@ucsc.edu>
2017-05-02 21:10:21 -0700

    changed code to generate psl data files in psl/data

--- code/script_reviewTextMentions.py
+++ code/script_reviewTextMentions.py
@@ -35,13 +35,13 @@ def getUnicoded(word):
 
 
 def writeToFile(array):
-	with open(os.path.join("..", "psl", "similarCuisine.txt"), 'w') as out_file:
+	with open(os.path.join("..", "psl", "data", "similarCuisine.txt"), 'w') as out_file:
 		for ele in array:
 			out_file.write(getUnicoded(ele[0]) + "\t" + getUnicoded(ele[1]) + "\n")
 
 
 def writeToFile2(dictionary):
-	with open(os.path.join("..", "psl", "restaurantLocation.txt"), 'w') as out_file:
+	with open(os.path.join("..", "psl", "data", "restaurantLocation.txt"), 'w') as out_file:
 		for business in dictionary.keys():
 			if dictionary[business][0] != '':
 				out_file.write(business + "\t" + getUnicoded(dictionary[business][0]) + "\n")
@@ -51,7 +51,7 @@ def writeToFile2(dictionary):
 				out_file.write(business + "\t" + getUnicoded(dictionary[business][2]) + "\n")
 
 def writeToFile3(dictionary):
-	with open(os.path.join("..", "psl", "restaurantCuisine.txt"), 'w') as out_file:
+	with open(os.path.join("..", "psl", "data", "restaurantCuisine.txt"), 'w') as out_file:
 		for k,v in dictionary.iteritems():
 			out_file.write(k + "\t" + v + "\n")
 
